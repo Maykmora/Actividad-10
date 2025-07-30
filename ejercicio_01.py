4dee025e55f88f490bae0faa5847a895ad5d2cfd
Maykmora <maykmora12@gmail.com>
2025-07-30 16:37:42 -0600

    Commit 3

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -1,11 +1,37 @@
 print("Bienvenido al programa")
-producto={}
+productos={}
 cantidad=int(input("Ingrese la cantidad de productos que desea agregar: "))
 for i in range(cantidad):
     print(f"PRODUCTO NO.{i+1}")
-    code=input("Ingrese el código del producto: ")
+    while True:
+        code=input("Ingrese el código del producto: ")
+        if code in productos:
+            print("El código ya esta registrado, intentelo de nuevo.")
+        else:
+            break
     name=input("Ingrese el nombre del producto: ")
     categoria=input("Ingrese la categoria del producto: ")
     size=input("Ingrese la talla del producto (S,M,L,XL): ")
-    price=int(input("Ingrese el precio unitario del producto: "))
-    stock=int(input("Ingrese la cantidad en stock:"))
+    while True:
+        price=int(input("Ingrese el precio unitario del producto: "))
+        if price>0:
+            break
+        else:
+            print("El precio debe ser mayor que 0, intente de nuevo.")
+
+    while True:
+        stock=int(input("Ingrese la cantidad en stock:"))
+        if price>0:
+            break
+        else:
+            print("El stock debe ser mayor que 0, intente de nuevo.")
+
+    productos[code]={
+        "nombre":name,
+        "categoria":categoria,
+        "talla":size,
+        "precio":price,
+        "stock":stock
+    }
+
+print(productos)
